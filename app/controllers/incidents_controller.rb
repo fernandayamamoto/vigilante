43d63a31285fa6f05f00d5e5061ba48a9c7082f3
Melline Prado <mellinevprado@hotmail.com>
2022-11-29 20:59:24 -0300

    update seed, update incident show

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,27 +1,23 @@
 class IncidentsController < ApplicationController
   def show
     @incident = Incident.find(params[:id])
-    # The `geocoded` scope filters only incidents with coordinates
-    @markers = @incidents.geocoded.map do |incident|
-    {
-      lat: incident.latitude,
-      lng: incident.longitude,
-      info_window: render_to_string(partial:
-
-      "info_window", locals: { incident: incident}),
-      image_url:
-      helpers.asset_url("https://res.cloudinary.com/dwvglguvp/image/upload/c_crop,h_210/v1669731800/pin_ladrao-removebg-preview_gdzctb.png")
-    }
+    @markers = [
+      {
+        lat: @incident.latitude,
+        lng: @incident.longitude,
+        info_window: render_to_string(partial: "pages/info_window", locals: {incident: @incident}),
+        image_url: helpers.asset_url("https://res.cloudinary.com/dwvglguvp/image/upload/c_crop,h_210/v1669731800/pin_ladrao-removebg-preview_gdzctb.png")
+      }
+    ]
   end
-end
 
   def new
     @incident = Incident.new
   end
 
   def create
-    #Falta a parte do geocoding
     @incident = Incident.new(incident_params)
+    @incident.user = current_user
     if @incident.save
       redirect_to incident_path(@incident)
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Incident.destroy_all
 User.destroy_all
-user = User.create!(user_id: 1, email: 'teste@teste.com', password: '123123')
-Incident.create!(user:1, location: "Rua Jericó, 193, Vila Madalena, São Paulo - SP, 05435-040", incident_type: "Le Wagon")
-Incident.create(user_id: 1, location: "Rua Fidalga, 809-839, Pinheiros, São Paulo - SP, 05432-000", incident_type: "Praça Éder Sader"),
-Incident.create(user_id: 1, location: "Rua Original, 87, Sumarezinho, São Paulo - SP, 05435-050", incident_type: "Bar do Sasha"),
-Incident.create(user_id: 1, location: "Rua Aspicuelta, 524, Vila Madalena, São Paulo - SP, 05433-011", incident_type: "O Pasquim Bar e Prosa"),
-Incident.create(user_id: 1, location: "Rua Deputado Lacerda Franco, 148, Pinheiros, São Paulo - SP, 05418-000", incident_type: "Golden Power Pinheiros By Fênix Hotéis"),
-Incident.create(user_id: 1, location: "Avenida Rebouças, 3970, Pinheiros, São Paulo - SP, 05402-600", incident_type: "Shopping Eldorado"),
-Incident.create(user_id: 1, location: "Avenida Brigadeiro Faria Lima, 2232, Jardim Paulistano, São Paulo - SP, 01451-000", incident_type: "Shopping Iguatemi"),
-Incident.create(user_id: 1, location: "Avenida Brigadeiro Faria Lima, 3477, Itaim Bibi, São Paulo - SP, 04538-133", incident_type: "Google"),
-Incident.create(user_id: 1, location: "Ibirapuera, São Paulo - SP, 04507-000", incident_type: "Parque Ibirapuera"),
-Incident.create(user_id: 1, location: "Rua Abílio Soares, 912, Paraíso, São Paulo - SP, 04005-003", incident_type: "Bráz Pizzaria"),
-Incident.create(user_id: 1, location: "Rua Martinho Prado, 128, Bela Vista, São Paulo - SP, 01306-040", incident_type: "Museu Judaico"),
-Incident.create(user_id: 1, location: "Rua Augusta, 200, Consolação, São Paulo - SP, 01303-050", incident_type: "Parque Augusta"),
-Incident.create(user_id: 1, location: "Praça da República, República, São Paulo - SP, 14786-079", incident_type: "Praça da República"),
-Incident.create(user_id: 1, location: "Terminal Rodoviário Barra Funda, Barra Funda, São Paulo - SP, 01156-001", incident_type: "Terminal Rodoviário Barra Funda"),
-Incident.create(user_id: 1, location: "Rua Brigadeiro Gavião Peixoto, 123, Lapa, São Paulo - SP, 05078-000", incident_type: "Hospital Albert Sabin"),
-Incident.create(user_id: 1, location: "Praça General Porto Carreiro, Jaguaré, São Paulo - SP, 05331-040", incident_type: "Praça General Porto Carreiro"),
-Incident.create(user_id: 1, location: "Praça Roberto Gomes Pedrosa, 1, Morumbi, São Paulo - SP, 05653-070", incident_type: "Estádio Morumbi"),
-Incident.create(user_id: 1, location: "Praça da Sé, Centro, São Paulo - SP, 01001-000", incident_type: "Catedral da Sé"),
-Incident.create(user_id: 1, location: "Avenida Paulista, 1578, Bela Vista, São Paulo - SP, 01310-200", incident_type: "MASP"),
-Incident.create(user_id: 1, location: "Rua Vinte e Cinco de Março, 848, Centro Histórico de São Paulo, São Paulo - SP, 01021-100", incident_type: "25 de Março"),
-Incident.create(user_id: 1, location: "Rua Roberto Simonsen, 136, Centro Histórico de São Paulo, São Paulo - SP, 01017-020", incident_type: "Museu Solar da Marquesa de Santos"),
-Incident.create(user_id: 1, location: "Praça da Luz, 2, Luz, São Paulo - SP, 01120-010", incident_type: "Pinacoteca de São Paulo"),
-Incident.create(user_id: 1, location: "Rua da Cantareira, 306, Centro Histórico de São Paulo, São Paulo - SP, 01024-900", incident_type: "Mercado Municipal"),
-Incident.create(user_id: 1, location: "Rua Medeiros de Albuquerque, 82, Jardim das Bandeiras, São Paulo - SP, 05436-060", incident_type: "Beco do Batman"),
-Incident.create(user_id: 1, location: "Largo São Bento, Centro Histórico de São Paulo, São Paulo - SP, 01029-010", incident_type: "Mosteiro de São Bento")
+user = User.create(email: 'teste@teste.com', password: '123123')
+Incident.create(user: user, location: "Rua Jericó, 193, Vila Madalena, São Paulo - SP, 05435-040", incident_type: "Le Wagon")
+Incident.create(user: user, location: "Rua Fidalga, 809-839, Pinheiros, São Paulo - SP, 05432-000", incident_type: "Praça Éder Sader")
+Incident.create(user: user, location: "Rua Original, 87, Sumarezinho, São Paulo - SP, 05435-050", incident_type: "Bar do Sasha")
+Incident.create(user: user, location: "Rua Aspicuelta, 524, Vila Madalena, São Paulo - SP, 05433-011", incident_type: "O Pasquim Bar e Prosa")
+Incident.create(user: user, location: "Rua Deputado Lacerda Franco, 148, Pinheiros, São Paulo - SP, 05418-000", incident_type: "Golden Power Pinheiros By Fênix Hotéis")
+Incident.create(user: user, location: "Avenida Rebouças, 3970, Pinheiros, São Paulo - SP, 05402-600", incident_type: "Shopping Eldorado")
+Incident.create(user: user, location: "Avenida Brigadeiro Faria Lima, 2232, Jardim Paulistano, São Paulo - SP, 01451-000", incident_type: "Shopping Iguatemi")
+Incident.create(user: user, location: "Avenida Brigadeiro Faria Lima, 3477, Itaim Bibi, São Paulo - SP, 04538-133", incident_type: "Google")
+Incident.create(user: user, location: "Ibirapuera, São Paulo - SP, 04507-000", incident_type: "Parque Ibirapuera")
+Incident.create(user: user, location: "Rua Abílio Soares, 912, Paraíso, São Paulo - SP, 04005-003", incident_type: "Bráz Pizzaria")
+Incident.create(user: user, location: "Rua Martinho Prado, 128, Bela Vista, São Paulo - SP, 01306-040", incident_type: "Museu Judaico")
+Incident.create(user: user, location: "Rua Augusta, 200, Consolação, São Paulo - SP, 01303-050", incident_type: "Parque Augusta")
+Incident.create(user: user, location: "Praça da República, República, São Paulo - SP, 14786-079", incident_type: "Praça da República")
+Incident.create(user: user, location: "Terminal Rodoviário Barra Funda, Barra Funda, São Paulo - SP, 01156-001", incident_type: "Terminal Rodoviário Barra Funda")
+Incident.create(user: user, location: "Rua Brigadeiro Gavião Peixoto, 123, Lapa, São Paulo - SP, 05078-000", incident_type: "Hospital Albert Sabin")
+Incident.create(user: user, location: "Praça General Porto Carreiro, Jaguaré, São Paulo - SP, 05331-040", incident_type: "Praça General Porto Carreiro")
+Incident.create(user: user, location: "Praça Roberto Gomes Pedrosa, 1, Morumbi, São Paulo - SP, 05653-070", incident_type: "Estádio Morumbi")
+Incident.create(user: user, location: "Praça da Sé, Centro, São Paulo - SP, 01001-000", incident_type: "Catedral da Sé")
+Incident.create(user: user, location: "Avenida Paulista, 1578, Bela Vista, São Paulo - SP, 01310-200", incident_type: "MASP")
+Incident.create(user: user, location: "Rua Vinte e Cinco de Março, 848, Centro Histórico de São Paulo, São Paulo - SP, 01021-100", incident_type: "25 de Março")
+Incident.create(user: user, location: "Rua Roberto Simonsen, 136, Centro Histórico de São Paulo, São Paulo - SP, 01017-020", incident_type: "Museu Solar da Marquesa de Santos")
+Incident.create(user: user, location: "Praça da Luz, 2, Luz, São Paulo - SP, 01120-010", incident_type: "Pinacoteca de São Paulo")
+Incident.create(user: user, location: "Rua da Cantareira, 306, Centro Histórico de São Paulo, São Paulo - SP, 01024-900", incident_type: "Mercado Municipal")
+Incident.create(user: user, location: "Rua Medeiros de Albuquerque, 82, Jardim das Bandeiras, São Paulo - SP, 05436-060", incident_type: "Beco do Batman")
+Incident.create(user: user, location: "Largo São Bento, Centro Histórico de São Paulo, São Paulo - SP, 01029-010", incident_type: "Mosteiro de São Bento")
