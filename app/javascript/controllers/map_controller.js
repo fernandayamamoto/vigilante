89b624a89571c28bd21af4fd61be1b97e096e124
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-11-29 16:33:19 -0300

    mapbox ok

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,32 +8,43 @@ export default class extends Controller {
   }
 
   connect() {
-     mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue
 
-     this.map = new mapboxgl.Map({
+    navigator.geolocation.getCurrentPosition((position)=>{
+      this.userLocation = [position.coords.longitude, position.coords.latitude]
+
+    })
+    this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: this.userLocation
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
-    }
-    #addMarkersToMap() {
-      this.markersValue.forEach((marker) => {
-        const popup = new
-        mapboxgl.Popup().setHTML(marker.info_window)
-        const customMarker = document.createElement("div")
-        customMarker.className = "marker"
-        customMarker.style.backgroundImage = `url('${marker.image_url}')`
-        customMarker.style.backgroundSize = "contain"
-        customMarker.style.width = "25px"
-        customMarker.style.height = "25px"
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
+  }
 
-        new mapboxgl.Marker(customMarker)
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup)
-          .addTo(this.map)
-      })
-    }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new
+      mapboxgl.Popup().setHTML(marker.info_window)
+      const customMarker = document.createElement("div")
+      customMarker.className = "marker"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = "contain"
+      customMarker.style.width = "40px"
+      customMarker.style.height = "40px"
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
 }
