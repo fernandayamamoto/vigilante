82caee429b3b40d009fb9ff6d0842cd2fcc91efa
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-11-29 11:50:22 -0300

    change controller name

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,13 +1,4 @@
 class IncidentsController < ApplicationController
   def index
-    @incidents = incident.all
-    # The `geocoded` scope filters only incidents with coordinates
-    @markers = @incidents.geocoded.map do |incident|
-      {
-        lat: incident.latitude,
-        lng: incident.longitude
-        info_window: render_to_string(partial:
-        "info_window", location: {incident: incident})
-      }
   end
 end
