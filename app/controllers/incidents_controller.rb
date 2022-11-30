230ed65c510e71d0985aa2309ecf6e7442e5c0c5
Aretha Lemes <aretha204@gmail.com>
2022-11-30 16:49:19 -0300

    MARCADORES PERSONALIZADOS QUASE OK. UHULLL.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,12 +1,22 @@
 class IncidentsController < ApplicationController
   def show
     @incident = Incident.find(params[:id])
+    if @incident.incident_type == "Roubo"
+      link_url = "https://res.cloudinary.com/deofzbzjp/image/upload/v1669836325/PIN_ROUBO-removebg-preview_ltnpyp.png"
+    elsif @incident.incident_type == "Furto"
+      link_url = "https://res.cloudinary.com/deofzbzjp/image/upload/v1669836325/PIN_FURTO-removebg-preview_jmgj8x.png"
+    elsif @incident.incident_type == "Assédio"
+      link_url = "https://res.cloudinary.com/deofzbzjp/image/upload/v1669836325/PIN_ASS%C3%89DIO-removebg-preview_ppm6kf.png"
+    else
+      link_url = "https://res.cloudinary.com/deofzbzjp/image/upload/v1669836325/PIN_OUTROS-removebg-preview_sisizq.png"
+    end
+
     @markers = [
       {
         lat: @incident.latitude,
         lng: @incident.longitude,
         info_window: render_to_string(partial: "pages/info_window", locals: {incident: @incident}),
-        image_url: helpers.asset_url("https://res.cloudinary.com/dwvglguvp/image/upload/c_crop,h_210/v1669731800/pin_ladrao-removebg-preview_gdzctb.png")
+        image_url: helpers.asset_url(link_url)
       }
     ]
   end
--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,5 @@ class Incident < ApplicationRecord
   belongs_to :user
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  TYPES = ["Roubo", "Furto", "Assédio", "Outros"]
 end
