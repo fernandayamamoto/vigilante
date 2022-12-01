9cd15874b64b29dc763c779490424314f7075fcf
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-12-01 18:49:09 -0300

    pundit fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(root_path)
-  end
-
   private
 
   def skip_pundit?
