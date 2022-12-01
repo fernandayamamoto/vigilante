6ba3d63692d86d835e95bf6e8c8ac9f52b6f1a75
Melline Prado <mellinevprado@hotmail.com>
2022-12-01 11:56:50 -0300

    update and destroy ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#home", as: 'home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
