94e8f5013ae94b327843175a81afddbcd6367f3b
Milena Neves Domingues <milena_neves95@hotmail.com>
2022-12-01 12:25:10 -0300

    pundit index, new, create, show

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,6 +1,7 @@
 class IncidentsController < ApplicationController
   def show
     @incident = Incident.find(params[:id])
+    authorize @incident
     if @incident.incident_type == "Roubo"
       link_url = "https://res.cloudinary.com/deofzbzjp/image/upload/v1669836325/PIN_ROUBO-removebg-preview_ltnpyp.png"
     elsif @incident.incident_type == "Furto"
@@ -23,11 +24,13 @@ class IncidentsController < ApplicationController
 
   def new
     @incident = Incident.new
+    authorize @incident
   end
 
   def create
     @incident = Incident.new(incident_params)
     @incident.user = current_user
+    authorize @incident
     if @incident.save
       redirect_to incident_path(@incident)
     else
---
+++ app/policies/incident_policy.rb
@@ -0,0 +1,16 @@
+class IncidentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+end
