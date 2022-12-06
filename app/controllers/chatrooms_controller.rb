db4452829450966e0894f6227a86a8f48a6978db
Melline Prado <mellinevprado@hotmail.com>
2022-12-06 12:52:27 -0300

    backend chat ok

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,4 +6,19 @@ after_action :skip_authorization
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def create
+    @incident = Incident.find(params[:incident_id])
+    @user = @incident.user
+    @user_two = current_user
+    @chatroom = Chatroom.create(user: @user, user_two: @user_two, name: "#{@user.nickname}-#{@user_two.nickname}")
+    redirect_to chatroom_path(@chatroom)
+  end
+
+  def index
+    @incident = Incident.find(params[:incident_id])
+    @user = @incident.user
+    @chatrooms_all = policy_scope(Chatroom)
+    @chatrooms = @chatrooms_all.where(user: @user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :incidents
-  resources :chatrooms, only: :show do
+  resources :incidents, only: [:show] do
+    resources :chatrooms, only: [:create, :index]
+  end
+  resources :chatrooms, only: [:show] do
     resources :messages, only: :create
   end
 end
