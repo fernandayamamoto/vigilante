41fafe81f8d3a35f54f21332904df08a9ec93ffc
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-12-07 17:11:14 -0300

    avatar no chat para terminar

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,6 +4,9 @@ after_action :skip_authorization
   def show
     # authorize @chatroom
     @chatroom = Chatroom.find(params[:id])
+    @incident = @chatroom.incident
+    @chatroom.user.avatar = "boy.png"
+    @chatroom.user_two.avatar = "avatar.png"
     @message = Message.new
     @chatrooms_all = policy_scope(Chatroom)
     @chatrooms = Chatroom.where(user: current_user).or(Chatroom.where(user_two: current_user))
@@ -12,7 +15,9 @@ after_action :skip_authorization
   def create
     @incident = Incident.find(params[:incident_id])
     @user = @incident.user
+    @user.avatar = "boy.png"
     @user_two = current_user
+    @user_two.avatar = "avatar.png"
     @chatroom = Chatroom.create(user: @user, user_two: @user_two, name: "#{@user.nickname}-#{@user_two.nickname}", incident: @incident)
     redirect_to chatroom_path(@chatroom)
   end
