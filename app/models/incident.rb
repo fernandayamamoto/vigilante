5c29f8fb496ebc20c42b6c619fb9fe8fb0a3ca24
Melline Prado <mellinevprado@hotmail.com>
2022-12-07 22:16:14 -0300

    validations form

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,4 +3,8 @@ class Incident < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   TYPES = ["Roubo", "Furto", "Assédio", "Outros"]
+
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :incident_type, presence: true, inclusion: { in: TYPES }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :chatrooms
+  validates :nickname, presence: true
 end
